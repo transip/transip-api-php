60d690c85299fa146a067c1f8c8e79d083ccc5c2
Sem Schilder <sschilder@transip.nl>
2019-06-13 15:50:30 +0200

    Add handover function for domainnames and bump versions

--- Transip/ForwardService.php
+++ Transip/ForwardService.php
@@ -16,7 +16,7 @@ class Transip_ForwardService
 	/** The SOAP service that corresponds with this class. */
 	const SERVICE = 'ForwardService';
 	/** The API version. */
-	const API_VERSION = '5.13';
+	const API_VERSION = '5.14';
 	/** @var SoapClient  The SoapClient used to perform the SOAP calls. */
 	protected static $_soapClient = null;
 
