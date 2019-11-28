5571a03ff4e2da7ad8e57329f88f3d171fe2e464
Stefan König <skonig@transip.nl>
2019-11-28 14:59:59 +0100

    Added Vps usage resource + fix GET params

---
+++ src/Transip/Api/Client/Entity/Vps/UsageDataNetwork.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Transip\Api\Client\Entity\Vps;
+
+class UsageDataNetwork extends UsageData
+{
+    /**
+     * @var float $mbitIn
+     */
+    public $mbitIn;
+
+    /**
+     * @var float $mbitOut
+     */
+    public $mbitOut;
+
+    public function getMbitIn(): float
+    {
+        return $this->mbitIn;
+    }
+
+    public function getMbitOut(): float
+    {
+        return $this->mbitOut;
+    }
+}
---
+++ src/Transip/Api/Client/Repository/Vps/UsageRepository.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Transip\Api\Client\Repository\Vps;
+
+use Transip\Api\Client\Entity\Vps\UsageDataCpu;
+use Transip\Api\Client\Entity\Vps\UsageDataDisk;
+use Transip\Api\Client\Entity\Vps\UsageDataNetwork;
+use Transip\Api\Client\Repository\ApiRepository;
+
+class UsageRepository extends ApiRepository
+{
+    public const TYPE_CPU = 'cpu';
+    public const TYPE_DISK = 'disk';
+    public const TYPE_NETWORK = 'network';
+
+    protected function getRepositoryResourceNames(): array
+    {
+        return ['vps', 'usage'];
+    }
+
+    /**
+     * @param string $vpsName
+     * @param array  $types
+     * @param int    $dateTimeStart
+     * @param int    $dateTimeEnd
+     * @return array
+     */
+    public function getByVpsName(
+        string $vpsName,
+        array $types = [],
+        int $dateTimeStart = 0,
+        int $dateTimeEnd = 0
+    ): array {
+        $parameters = [];
+        if (count($types) > 0) {
+            $parameters['types'] = implode(',',$types);
+        }
+        if ($dateTimeStart > 0) {
+            $parameters['dateTimeStart'] = $dateTimeStart;
+        }
+        if ($dateTimeEnd > 0) {
+            $parameters['dateTimeEnd'] = $dateTimeEnd;
+        }
+
+        $usages          = [];
+        $response        = $this->httpClient->get($this->getResourceUrl($vpsName), $parameters);
+        $usageTypesArray = $response['usage'] ?? [];
+
+        foreach ($usageTypesArray as $usageType => $usageArray) {
+            switch ($usageType) {
+                case self::TYPE_CPU:
+                    foreach ($usageArray as $usage) {
+                        $usages[$usageType][] = new UsageDataCpu($usage);
+                    }
+                    break;
+                case self::TYPE_DISK:
+                    foreach ($usageArray as $usage) {
+                        $usages[$usageType][] = new UsageDataDisk($usage);
+                    }
+                    break;
+                case self::TYPE_NETWORK:
+                    foreach ($usageArray as $usage) {
+                        $usages[$usageType][] = new UsageDataNetwork($usage);
+                    }
+                    break;
+            }
+        }
+
+        return $usages;
+    }
+}
