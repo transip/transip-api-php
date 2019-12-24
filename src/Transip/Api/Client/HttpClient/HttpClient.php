eae362f1b2eb566e735503a2a30eac57b0210135
Stefan König <skonig@transip.nl>
2019-12-24 13:41:40 +0100

    Added private key fingerprint to invalidate the token in cache when key changes

--- src/Transip/Api/Client/HttpClient/HttpClient.php
+++ src/Transip/Api/Client/HttpClient/HttpClient.php
@@ -9,6 +9,9 @@ use Transip\Api\Client\TransipAPI;
 
 abstract class HttpClient
 {
+    public const TOKEN_CACHE_KEY = 'token';
+    public const KEY_FINGERPRINT_CACHE_KEY = 'key-fingerprint';
+
     /**
      * @var AuthRepository $authRepository
      */
@@ -63,13 +66,46 @@ abstract class HttpClient
             $token = $this->authRepository->createToken($this->login, $this->privateKey, $this->generateWhitelistOnlyTokens);
             $this->setToken($token);
 
-            // Save new token to a temporary file
-            $cacheItem = $this->cache->getItem(TransipAPI::TEMP_TOKEN_FILE_NAME);
+            // Save new token to cache
+            $cacheItem = $this->cache->getItem(self::TOKEN_CACHE_KEY);
             $cacheItem->set($token);
             $cacheItem->expiresAfter($this->authRepository->getExpiryTime());
-
+            $this->cache->save($cacheItem);
+            // Save private key fingerprint to cache
+            $cacheItem = $this->cache->getItem(self::KEY_FINGERPRINT_CACHE_KEY);
+            $cacheItem->set($this->getFingerPrintFromKey($this->privateKey));
+            $cacheItem->expiresAfter($this->authRepository->getExpiryTime());
             $this->cache->save($cacheItem);
         }
+    }
+
+    public function getTokenFromCache()
+    {
+        $cachedToken = $this->cache->getItem(self::TOKEN_CACHE_KEY);
+        $cachedKeyFP = $this->cache->getItem(self::KEY_FINGERPRINT_CACHE_KEY);
+
+        if ($cachedToken->isHit() && $cachedKeyFP->isHit()) {
+            $storedKeyFP = $cachedKeyFP->get();
+            $storedToken = $cachedToken->get();
+
+            // check if the used private key is still the same, else invalidate the cache
+            if ($this->getFingerPrintFromKey($this->privateKey) === $storedKeyFP) {
+                $this->setToken($storedToken);
+            } else {
+                $this->clearCache();
+            }
+        }
+    }
+
+    private function getFingerPrintFromKey(string $privateKey): string
+    {
+        return hash('SHA512', $privateKey);
+    }
+
+    public function clearCache(): void
+    {
+        $this->cache->deleteItem(self::TOKEN_CACHE_KEY);
+        $this->cache->deleteItem(self::KEY_FINGERPRINT_CACHE_KEY);
     }
 
     public function setEndpoint(string $endpoint): void
