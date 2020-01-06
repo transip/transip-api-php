0926efc4fdc4f685c2d8126b6f7780567b132ae7
Samih Soylu <ssoylu@transip.nl>
2020-01-06 17:12:08 +0100

    Created a usage repository

---
+++ src/Transip/Api/Client/Repository/BigStorage/UsageRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Transip\Api\Client\Repository\BigStorage;
+
+use Transip\Api\Client\Entity\Vps\UsageDataDisk;
+use Transip\Api\Client\Repository\ApiRepository;
+use Transip\Api\Client\Repository\BigStorageRepository;
+
+class UsageRepository extends ApiRepository
+{
+    public const RESOURCE_NAME = 'usage';
+
+    protected function getRepositoryResourceNames(): array
+    {
+        return [BigStorageRepository::RESOURCE_NAME, self::RESOURCE_NAME];
+    }
+
+    protected function getUsageStatistics(
+        string $bigStorageName,
+        int $dateTimeStart = 0,
+        int $dateTimeEnd = 0,
+        string $dateTimeFormat = null
+    ): array {
+        $usages     = [];
+        $parameters = [];
+
+        if ($dateTimeStart > 0) {
+            $parameters['dateTimeStart'] = $dateTimeStart;
+        }
+        if ($dateTimeEnd > 0) {
+            $parameters['dateTimeEnd'] = $dateTimeEnd;
+        }
+        if ($dateTimeFormat !== null) {
+            $parameters['dateTimeFormat'] = $dateTimeFormat;
+        }
+
+        $response = $this->httpClient->get($this->getResourceUrl($bigStorageName), $parameters);
+        $usageStatistics = $response['usage'] ?? null;
+
+        foreach($usageStatistics as $usage) {
+            $usages[] = new UsageDataDisk($usage);
+        }
+
+        return $usages;
+    }
+}
