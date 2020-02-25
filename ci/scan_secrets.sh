c5d68af48622d21be842c3864171ab7064264643
Samih Soylu <ssoylu@transip.nl>
2020-02-25 11:01:40 +0100

    Removed gitdb2 install line from script

--- ci/scan_secrets.sh
+++ ci/scan_secrets.sh
@@ -11,7 +11,6 @@ fi
 
 if ! which trufflehog; then
   echo "# Installing trufflehog before scanning"
-  pip install gitdb2==3.0.0 # todo remove after https://github.com/dxa4481/truffleHog/issues/200 has been resovled
   pip -q install trufflehog
 fi
 
