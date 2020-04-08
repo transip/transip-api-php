3cc518867ffaf547d7792093aa55744c410808dc
Samih Soylu <ssoylu@transip.nl>
2020-04-08 10:27:40 +0200

    Now taken the 'v' in to consideration in the tag variable, when it is provided

--- ci/ValidateTagVersionConsistency.php
+++ ci/ValidateTagVersionConsistency.php
@@ -10,9 +10,11 @@ exec('composer dump-autoload');
 require_once(BASE_PATH . '/vendor/autoload.php');
 
 if (!isset($argv[1])) {
-    throw new RuntimeException("An argument must be provided, usage example: php {$argv[0]} 6.1.3");
+    throw new RuntimeException("An argument must be provided, usage example: php {$argv[0]} v6.1.3");
 }
 
-if (TransipAPI::TRANSIP_API_LIBRARY_VERSION !== $argv[1]) {
-    throw new RuntimeException("Version in TransipAPI class must be {$argv[1]}");
+$newVersion = ltrim($argv[1], 'v');
+
+if (TransipAPI::TRANSIP_API_LIBRARY_VERSION !== $newVersion) {
+    throw new RuntimeException("Version in TransipAPI class must be updated to: {$newVersion}");
 }
