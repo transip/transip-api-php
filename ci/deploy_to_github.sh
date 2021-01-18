db064411b56887721dfeae37accea9d60a623d93
Dion Bosschieter <dbosschieter@transip.nl>
2021-01-18 11:00:46 +0100

    Fix deploying shallow repositories to github

--- ci/deploy_to_github.sh
+++ ci/deploy_to_github.sh
@@ -8,6 +8,14 @@ if [ -z "$GITHUB_TOKEN" ] ; then
   echo "we need a github token in order to proceed";
   exit 1;
 fi
+if [ -z "$CI_REPOSITORY_URL" ] ; then
+  echo "we need a repository url to proceed";
+  exit 1;
+fi
+
+# force a full clone, instead of expecting a non shallow cloned repository
+git clone $CI_REPOSITORY_URL /tmp/repository
+cd /tmp/repository
 
 # Remove the default origin/HEAD -> origin/master ref
 # as github shows this as new branch
