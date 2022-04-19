cca2c9c377774150a90ab91aaef166fba9ad80f6
Jordi Donadeu <donadeuj@transip.nl>
2022-04-19 09:23:53 +0000

    449 request colo access

---
+++ src/Repository/Colocation/AccessRequestRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Transip\Api\Library\Repository\Colocation;
+
+use Transip\Api\Library\Entity\Colocation\AccessRequest;
+use Transip\Api\Library\Repository\ApiRepository;
+use Transip\Api\Library\Repository\ColocationRepository;
+
+class AccessRequestRepository extends ApiRepository
+{
+    public const RESOURCE_NAME = 'access-request';
+
+    protected function getRepositoryResourceNames(): array
+    {
+        return [ColocationRepository::RESOURCE_NAME, self::RESOURCE_NAME];
+    }
+
+    public function create(AccessRequest $accessRequest): void
+    {
+        $url        = $this->getResourceUrl($accessRequest->getColoName());
+        $parameters = ['accessRequest' => $accessRequest];
+        $this->httpClient->post($url, $parameters);
+    }
+}
