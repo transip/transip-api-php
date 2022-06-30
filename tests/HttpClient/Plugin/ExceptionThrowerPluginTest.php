161d19508b2dacaf787ff7d6ae78ad8855e6ae16
Sem Schilder <sem@tropical.email>
2022-06-30 13:25:06 +0200

    Add exception thrower test

--- tests/HttpClient/Plugin/ExceptionThrowerPluginTest.php
+++ tests/HttpClient/Plugin/ExceptionThrowerPluginTest.php
@@ -14,7 +14,20 @@ use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Transip\Api\Library\Exception\ApiException;
 use Transip\Api\Library\Exception\HttpBadResponseException;
+use Transip\Api\Library\Exception\HttpRequest\AccessTokenException;
+use Transip\Api\Library\Exception\HttpRequest\BadResponseException;
+use Transip\Api\Library\Exception\HttpRequest\BadResponseTimeoutException;
+use Transip\Api\Library\Exception\HttpRequest\ConflictException;
+use Transip\Api\Library\Exception\HttpRequest\ForbiddenException;
 use Transip\Api\Library\Exception\HttpRequest\InternalServerErrorException;
+use Transip\Api\Library\Exception\HttpRequest\MethodNotAllowedException;
+use Transip\Api\Library\Exception\HttpRequest\NotAcceptableException;
+use Transip\Api\Library\Exception\HttpRequest\NotFoundException;
+use Transip\Api\Library\Exception\HttpRequest\NotImplementedException;
+use Transip\Api\Library\Exception\HttpRequest\RateLimitException;
+use Transip\Api\Library\Exception\HttpRequest\TooManyBadResponseException;
+use Transip\Api\Library\Exception\HttpRequest\UnauthorizedException;
+use Transip\Api\Library\Exception\HttpRequest\UnprocessableEntityException;
 use Transip\Api\Library\HttpClient\Plugin\ExceptionThrowerPlugin;
 
 final class ExceptionThrowerPluginTest extends TestCase
@@ -75,10 +88,67 @@ final class ExceptionThrowerPluginTest extends TestCase
                 'response' => new Response(500, [], json_encode(['error' => 'Internal Server Error'])),
                 'exception' => InternalServerErrorException::class
             ],
-            '410 Response' => [
-                'response' => new Response(410, [], json_encode(['error' => 'Some Error Description'])),
-                'exception' => HttpBadResponseException::class,
+            '400 Response with message' => [
+                'response' => new Response(400, [], json_encode(['error' => 'Some Error Description'])),
+                'exception' => BadResponseException::class,
                 'message' => 'Some Error Description',
+            ],
+            '400 Response empty body' => [
+                'response' => new Response(400, [], ''),
+                'exception' => BadResponseException::class,
+            ],
+            '401 Response' => [
+                'response' => new Response(401, [], ''),
+                'exception' => UnauthorizedException::class,
+            ],
+            '401 Response msg 1' => [
+                'response' => new Response(401, [], json_encode(['error' => 'Your access token has been revoked.'])),
+                'exception' => AccessTokenException::class,
+            ],
+            '401 Response msg 2' => [
+                'response' => new Response(401, [], json_encode(['error' => 'Your access token has expired.'])),
+                'exception' => AccessTokenException::class,
+            ],
+            '403 Response' => [
+                'response' => new Response(403, []),
+                'exception' => ForbiddenException::class,
+            ],
+            '404 Response' => [
+                'response' => new Response(404, []),
+                'exception' => NotFoundException::class,
+            ],
+            '405 Response' => [
+                'response' => new Response(405, []),
+                'exception' => MethodNotAllowedException::class,
+            ],
+            '406 Response' => [
+                'response' => new Response(406, []),
+                'exception' => NotAcceptableException::class,
+            ],
+            '408 Response' => [
+                'response' => new Response(408, []),
+                'exception' => BadResponseTimeoutException::class,
+            ],
+            '409 Response' => [
+                'response' => new Response(409, []),
+                'exception' => ConflictException::class,
+            ],
+            '422 Response' => [
+                'response' => new Response(422, []),
+                'exception' => UnprocessableEntityException::class,
+            ],
+            '429 Response' => [
+                'response' => new Response(429, []),
+                'exception' => RateLimitException::class,
+            ],
+            '501 Response' => [
+                'response' => new Response(501, []),
+                'exception' => NotImplementedException::class,
+            ],
+            // Should catch the generic error
+            '599 Response' => [
+                'response' => new Response(599, []),
+                'exception' => HttpBadResponseException::class,
             ],
         ];
 
