fb8ab0ac0f99b6db34eb666d97967eecc865424f
TransIP Developer <no-reply@transip.nl>
2023-08-28 12:09:05 +0200

    Add support for Kubernetes cluster upgrades

---
+++ src/Repository/Kubernetes/Cluster/ReleaseRepository.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Transip\Api\Library\Repository\Kubernetes\Cluster;
+
+use Transip\Api\Library\Entity\Kubernetes\Cluster\Release as KubernetesRelease;
+use Transip\Api\Library\Repository\ApiRepository;
+
+class ReleaseRepository extends ApiRepository
+{
+    public const RESOURCE_NAME = 'releases';
+    public const RESOURCE_PARAMETER_SINGULAR = 'release';
+    public const RESOURCE_PARAMETER_PLURAL   = 'releases';
+
+    /**
+     * @return KubernetesRelease[]
+     */
+    public function getAll(string $clusterName): array
+    {
+        return $this->getReleases($clusterName, []);
+    }
+
+    /**
+     * @return KubernetesRelease[]
+     */
+    public function getCompatibleUpgrades(string $clusterName): array
+    {
+        return $this->getReleases($clusterName, ['isCompatibleUpgrade' => true]);
+    }
+
+    public function getByVersion(string $clusterName, string $version): KubernetesRelease
+    {
+        $response = $this->httpClient->get($this->getResourceUrl($clusterName, $version));
+
+        return new KubernetesRelease($this->getParameterFromResponse($response, self::RESOURCE_PARAMETER_SINGULAR));
+    }
+
+    /**
+     * @param string $clusterName
+     * @param array<string, mixed> $query
+     * @return KubernetesRelease[]
+     */
+    private function getReleases(string $clusterName, array $query = []): array
+    {
+        $releases = [];
+
+        $response = $this->httpClient->get($this->getResourceUrl($clusterName), $query);
+        $releasesArrays = $this->getParameterFromResponse($response, self::RESOURCE_PARAMETER_PLURAL);
+
+        foreach ($releasesArrays as $releasesArray) {
+            $releases[] = new KubernetesRelease($releasesArray);
+        }
+
+        return $releases;
+    }
+}
